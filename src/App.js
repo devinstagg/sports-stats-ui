ee914d9af90cb96d5361a33bc6e520de8b34d973
Devin Stagg <devinstagg7@gmail.com>
2020-04-14 21:16:18 -0600

    updating app js to include the correct functions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    nflTeams: [],
+  }
+
+  
+  showNflTeams = () => {
+    return this.state.nflTeams.map((nflTeam) => {
+      console.log(nflTeam)
+      return (<div key={this._id}>{JSON.stringify(nflTeam)}</div>)
+    })
+  }
+
+  getNflTeams = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/nfl-teams`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'accept': 'application/json'
+      }
+    })
+    const successfulGet = response.status === 200 || 304
+
+    if (successfulGet) {
+      const nflTeams = await response.json()
+
+      this.setState({ nflTeams })
+    } else {
+      console.log(response)
+    }
+  }
+
+  componentDidMount() {
+    this.getNflTeams()
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        {this.showNflTeams()}
+      </div>
+    );
+  }
 }
 
-export default App;
+
