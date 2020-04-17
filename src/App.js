e0d3fa7450482ba775180209189dd73ac24cbc19
Devin Stagg <devinstagg7@gmail.com>
2020-04-17 09:19:19 -0600

    updating styling and starting input section

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import './App.css';
 export default class App extends Component {
   state = {
     nflTeams: [],
+    userInput: {
+      location: '',
+      teamMascot: '',
+      conference: '',
+      division: '',
+      headCoach: '',
+      regularSeasonWins2019: '',
+      playoffs: ''
+    }
   }
   showNflTeams = () => {
     return this.state.nflTeams.map((nflTeam) => {
       return (
-        <div key={this._id} className="nfl">
-          <section>
-            <h2>{nflTeam.location} {nflTeam.team_mascot}</h2>
-            <p>Conference & Division: {nflTeam.conference} {nflTeam.division}</p>
-            <p>2020 Head Coach: {nflTeam.head_coach}</p>
-          </section>
+        <div key={this._id}>
+          <h2>{nflTeam.location} {nflTeam.teamMascot}</h2>
+          <p>Conference & Division: {nflTeam.conference} {nflTeam.division}</p>
+          <p>2020 Head Coach: {nflTeam.headCoach}</p>
+          <p>2019 Wins: {nflTeam.regularSeasonWins2019}</p>
+          <p>Playoffs? {nflTeam.playoffs}</p>
         </div>
       )
     })
@@ -41,9 +50,16 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
-        <h1>NFL Teams</h1>
-        {this.showNflTeams()}
+      <div className="hub">
+        <section className="adding">
+          <h2 className="section-header">Add Your Team</h2>
+          <label htmlFor="location-input">Location (Example - New England for the Patriots): </label>
+          <input id="location-input" type="text" value={this.state.userInput.location} onChange={this.locationChanged}/>
+        </section>
+        <section className="teams">
+          <h2 className="section-header">NFL Teams</h2>
+          {this.showNflTeams()}
+        </section>
       </div>
     );
   }
